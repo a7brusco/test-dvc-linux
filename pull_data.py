f80e8e5264ea242f779fefaa32866d040281bc0c
Antoine Brusco <a7brusco@enib.fr>
2022-01-27 14:04:40 +0100

    l

--- pull_data.py
+++ pull_data.py
@@ -3,3 +3,4 @@ import os
 dna_data_path = os.environ.get("DNA_DATA_PATH")
 os.system(f"dvc remote modify main url {dna_data_path}")
 os.system(f"dvc cache dir {dna_data_path}/cache")
+os.system("dvc pull")
